36a11fb09e2d71542d02f2b65cbaa7b7a1669410
ola abaza <olaabaza@olas-Mac.local>
2021-04-21 11:00:57 -0700

    add constants class

---
+++ SportsApp/Constants/URLs.swift
@@ -0,0 +1,15 @@
+//
+//  URLs.swift
+//  SportsApp
+//
+//  Created by ola abaza on 4/21/21.
+//  Copyright © 2021 Norhan. All rights reserved.
+//
+
+import Foundation
+struct URLs {
+    
+    public static var getSportsListURL : String =
+    "https://www.thesportsdb.com/api/v1/json/1/all_sports.php"
+    
+}
